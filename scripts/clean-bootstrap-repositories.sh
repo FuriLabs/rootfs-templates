43c37ff39e7793b64f319ad1adcfa80b52377bfa
Bardia Moshiri <fakeshell@bardia.tech>
2024-03-07 23:40:46 -0500

    treewide: use furilabs repos
    Signed-off-by: Bardia Moshiri <fakeshell@bardia.tech>

--- scripts/clean-bootstrap-repositories.sh
+++ scripts/clean-bootstrap-repositories.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Remove extrepos
-rm -f /etc/apt/sources.list.d/extrepo_droidian.sources
+rm -f /etc/apt/sources.list.d/extrepo_furilabs.sources
 rm -f /etc/apt/sources.list.d/extrepo_mobian.sources
-rm -f /var/lib/extrepo/keys/hybris-mobian.asc
+rm -f /var/lib/extrepo/keys/furilabs.asc
 rm -f /var/lib/extrepo/keys/mobian.asc
 
 # Nuke /etc/apt/sources.list
