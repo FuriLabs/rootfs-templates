e0eee9cdf5dc0beb717984b14ce9cc011622f215
Bardia Moshiri <bardia@furilabs.com>
2025-04-19 23:35:35 -0400

    treewide: rename to furios and drop sideload and recovery flashables
    Signed-off-by: Bardia Moshiri <bardia@furilabs.com>

--- scripts/clean-bootstrap-repositories.sh
+++ scripts/clean-bootstrap-repositories.sh
@@ -2,9 +2,7 @@
 
 # Remove extrepos
 rm -f /etc/apt/sources.list.d/extrepo_furilabs.sources
-rm -f /etc/apt/sources.list.d/extrepo_mobian.sources
 rm -f /var/lib/extrepo/keys/furilabs.asc
-rm -f /var/lib/extrepo/keys/mobian.asc
 
 # Nuke /etc/apt/sources.list
 > /etc/apt/sources.list
