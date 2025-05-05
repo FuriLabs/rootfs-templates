f5bcbbda3eaf21b610bcbbda778ab7f5b0578732
Bardia Moshiri <bardia@furilabs.com>
2025-05-04 21:18:40 -0400

    setup-gsi: remove firmware loading udev rule on image creation
    initially i thought this was not needed, but we do need this for the very first boot for halium to initialize everything first and not conflict with udevd. on a udev upgrade this will come back but that doesn't really matter since ueventd has already done its thing
    
    Signed-off-by: Bardia Moshiri <bardia@furilabs.com>

--- scripts/setup-gsi.sh
+++ scripts/setup-gsi.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# halium firmware loader conflicts with ueventd one
+if [ -f "/usr/lib/udev/rules.d/50-firmware.rules" ]; then
+    rm /usr/lib/udev/rules.d/50-firmware.rules
+fi
+
 # create android users and groups
 systemd-sysusers
 
