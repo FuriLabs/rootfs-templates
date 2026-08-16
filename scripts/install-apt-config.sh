270dca59d4930e4e5b135a6159536da1294b41e3
Bardia Moshiri <bardia@furilabs.com>
2026-08-16 01:38:42 +0000

    scripts: switch device repo to sync
    Signed-off-by: Bardia Moshiri <bardia@furilabs.com>

--- scripts/install-apt-config.sh
+++ scripts/install-apt-config.sh
@@ -3,6 +3,6 @@
 export DEBIAN_FRONTEND=noninteractive
 
 apt-get -o "Acquire::https::Verify-Peer=false" update
-apt-get -o "Acquire::https::Verify-Peer=false" install ca-certificates furios-apt-config furios-apt-config-krypton furios-archive-keyring -y
+apt-get -o "Acquire::https::Verify-Peer=false" install ca-certificates furios-apt-config furios-apt-config-sync furios-archive-keyring -y
 
 exit 0
